f951ce2cebf071eea783a6619bb197f9f8106394
Abimanyu2000 <abimanyuganesan2000@gamil.com>
2025-03-31 15:38:10 +0530

    Data transformation implemented and artifacts were removed from repo tracking

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts", "train.csv")
@@ -21,6 +24,8 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         try:
             df=pd.read_csv('notebooks/data/StudentsPerformance.csv')
+            df.columns=[col.replace(" ","_") for col in df.columns]
+    
             logging.info("Dataset converted to dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
@@ -32,6 +37,8 @@ class DataIngestion:
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            
 
             logging.info("Ingestion of data is completed")
 
@@ -47,4 +54,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
